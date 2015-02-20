35c751b55ee5689b598a34e3bf9e5739f287e0d7
Edoardo Negri <edne@gmx.com>
2015-02-20 15:21:00 +0100

    pep8 depthbuffer

--- gletools/depthbuffer.py
+++ gletools/depthbuffer.py
@@ -8,23 +8,25 @@
 from __future__ import with_statement
 
 from ctypes import byref
-from contextlib import nested
 
-from gletools.gl import *
+import gletools.gl as gl
 from .util import Context
 
 __all__ = ['Depthbuffer']
 
+
 class Depthbuffer(Context):
-    _get = GL_RENDERBUFFER_BINDING_EXT
+    _get = gl.GL_RENDERBUFFER_BINDING_EXT
 
     def bind(self, id):
-        glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, id)
-        
+        gl.glBindRenderbufferEXT(gl.GL_RENDERBUFFER_EXT, id)
+
     def __init__(self, width, height):
         Context.__init__(self)
-        id = GLuint()
-        glGenRenderbuffersEXT(1, byref(id))
+        id = gl.GLuint()
+        gl.glGenRenderbuffersEXT(1, byref(id))
         self.id = id.value
         with self:
-            glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, GL_DEPTH_COMPONENT, width, height)
+            gl.glRenderbufferStorageEXT(gl.GL_RENDERBUFFER_EXT,
+                                        gl.GL_DEPTH_COMPONENT,
+                                        width, height)
